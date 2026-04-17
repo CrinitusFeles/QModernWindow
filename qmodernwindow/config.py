f5d31f2f6fe704a8ad58c0a0424883a1af0c3989
CrinitusFeles <crinitusfeles@gmail.com>
2026-04-17 20:00:14 +0700

    added app_config conflict dialog

--- qmodernwindow/config.py
+++ qmodernwindow/config.py
@@ -1,8 +1,31 @@
 from pathlib import Path
+import sys
 from typing import Self
 
+from PyQt6 import QtWidgets
 from loguru import logger
 from pydantic import BaseModel
+
+
+def show_dialog():
+    msg = QtWidgets.QMessageBox()
+    msg_btn = QtWidgets.QMessageBox.StandardButton
+    msg.setStandardButtons(msg_btn.Yes | msg_btn.No)
+    msg.setWindowTitle('Внимание!')
+    msg.setText('Невозможно загрузить файл конфигурации из-за\n'\
+                'неправильной/обновленной структуры.\n\n'\
+                'Для использования текущего файла конфигурации необходимо исправить \n'\
+                'некорректную структуру вручную, после чего перезапустить программу.\n\n'\
+                'Нажмите "Yes" для перезаписи существующего файла новым валидным.\n'\
+                'Все изменения в текушем файле будут потеряны.')
+    msg.setIcon(QtWidgets.QMessageBox.Icon.Warning)
+    style = msg.style()
+    if style:
+        icon = style.standardIcon(QtWidgets.QStyle.StandardPixmap.SP_MessageBoxWarning)
+        msg.setWindowIcon(icon)
+
+    btn = msg.exec()
+    return btn == msg_btn.Yes
 
 
 class GUI_Config(BaseModel):
@@ -27,7 +50,17 @@ class GUI_Config(BaseModel):
         if config_path.exists():
             with open(config_path, 'r+', encoding='utf-8') as file:
                 raw_config: str = file.read()
-                return cls.model_validate_json(raw_config)
+                try:
+                    return cls.model_validate_json(raw_config)
+                except Exception as err:
+                    logger.exception(err)
+                    if show_dialog():
+                        config = cls()
+                        config.save_config()
+                        return config
+                    else:
+                        sys.exit(0)
+
         else:
             logger.debug('GUI Config was not found. Created default config')
             config = cls()
